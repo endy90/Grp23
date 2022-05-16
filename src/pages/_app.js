118c5874cfd1949523c3af6ea00faa3181a58cf7
thomas78125 <thomas78125@gmail.com>
2022-05-16 11:33:02 +0200

    header

---
+++ src/pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/styles.scss';
+import MainLayout from '../layout/MainLayout';
+import '../../dist/output.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  )
+}
+
+export default MyApp
